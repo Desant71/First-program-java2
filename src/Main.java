6da340da02e2d44b579e96238826c5e92c4074fc
Desant71 <desant31081992@gmail.com>
2023-03-25 03:27:30 +0300

    Add file

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nikita Filyakin");
+        String name = "Nikita";
+        String surname = "Filyakin";
+        int income = 50000;
+        int spending = 30000;
+        System.out.println(surname + " " + name + "," + "surplus:" + (income - spending));
+    }
+}
