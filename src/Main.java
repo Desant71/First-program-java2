ad3161073ff0329514c09a854bbee52f2d7ac761
Desant71 <desant31081992@gmail.com>
2023-03-25 03:30:17 +0300

    Add file2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nikita Filyakin");
-        String name = "Nikita";
+        System.out.println("Nikita Filyakin");//выводим надпись
+        String name = "Nikita";//переменная стринг
         String surname = "Filyakin";
-        int income = 50000;
+        int income = 50000;//переменная инт
         int spending = 30000;
-        System.out.println(surname + " " + name + "," + "surplus:" + (income - spending));
+        System.out.println(surname + " " + name + "," + "surplus:" + (income - spending));//выводим строку
     }
 }
